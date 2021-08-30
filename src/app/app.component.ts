287e2f3b17cfb3962e23aaa8c3d471c609cc5f43
helping083 <helping083@gmail.com>
2021-08-30 12:11:04 +0300

    deleted the old code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { AuthServiceService } from './auth/auth-service.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-  loadedFeature: string = 'recipes';
-
-  constructor(private authService: AuthServiceService) {
-  }
-
-  ngOnInit() {
-    this.authService.autoLogin();
-  }
+export class AppComponent  {
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,67 +3,18 @@ import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule }   from '@angular/forms';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { RecipesComponent } from './recipes/recipes.component';
-import { RecipesListComponent } from './recipes/recipes-list/recipes-list.component';
-import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
-import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-import { ShoppingListEditComponent } from './shopping-list/shopping-list-edit/shopping-list-edit.component';
-import { RecipeItemComponent } from './recipes/recipes-list/recipe-item/recipe-item.component';
-import { BoldDirective } from './bold.directive';
-import { DropdownDirective } from './shared/dropdown.directive';
-import { ShoppingListService } from './shopping-list/shopping-list.service';
-import { AppRoutingModule } from './app-routing.module';
-import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
-import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
-import { TryFormComponent } from './try-form/try-form.component';
-import { ReactFormsComponent } from './react-forms/react-forms.component';
-import { RecipeService } from './recipes/recipe.service';
-import { DataStorageService } from './shared/data-storage.service';
-import { RecipesResolverService } from './recipes/recipes-resolver.service';
-import { AuthComponentComponent } from './auth/auth-component/auth-component.component';
-import { AuthServiceService } from '../app/auth/auth-service.service';
-import { LoadingSpinnerComponent } from './loading-spinner/loading-spinner.component';
-import { AuthInterceptorService } from '../app/auth/auth-interceptor.service';
-import { HeroesModule } from './heroes/heroes.module';
-import { RxjsTestComponent } from './rxjs-test/rxjs-test.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    RecipesComponent,
-    RecipesListComponent,
-    RecipeDetailComponent,
-    ShoppingListComponent,
-    ShoppingListEditComponent,
-    RecipeItemComponent,
-    BoldDirective,
-    DropdownDirective,
-    RecipeStartComponent,
-    RecipeEditComponent,
-    TryFormComponent,
-    ReactFormsComponent,
-    AuthComponentComponent,
-    LoadingSpinnerComponent,
-    RxjsTestComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HeroesModule,
-    AppRoutingModule,
     ReactiveFormsModule,
     HttpClientModule
   ],
-  providers: [
-    ShoppingListService, 
-    RecipeService, 
-    DataStorageService, 
-    RecipesResolverService, 
-    AuthServiceService,
-    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptorService, multi: true }
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
